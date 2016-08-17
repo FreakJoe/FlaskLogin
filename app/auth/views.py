00a7bae35bf5b1309f513e14f0d8c66550f2d9b2
FreakJoe <sandmensch933@web.de>
2016-08-17 15:53:29 +0200

    It does work indeed

--- app/auth/views.py
+++ app/auth/views.py
@@ -14,7 +14,7 @@ def login():
 		print(user)
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.remember_me.data)
-			return redirect(request.args.get('next') or url_for('main.index'))
+			return redirect(request.args.get('next') or url_for('index'))
 
 		print('Failed to log in')
 
@@ -24,4 +24,4 @@ def login():
 @login_required
 def logout():
 	logout_user()
-	return redirect(url_for('main.index'))
+	return redirect(url_for('index'))

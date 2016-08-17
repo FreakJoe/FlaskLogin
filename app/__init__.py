42406c699ccca5a2135bb570c0a76389eb071190
FreakJoe <sandmensch933@web.de>
2016-08-17 14:36:07 +0200

    Fixed some issues with the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -17,10 +17,7 @@ app = CustomFlask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-def create_app(config_name):
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-	return app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
 from app import views, models

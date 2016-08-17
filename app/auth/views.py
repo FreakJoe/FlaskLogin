8830479731c1fa29c996851246e7c86a8619e556
FreakJoe <sandmensch933@web.de>
2016-08-17 19:25:44 +0200

    Added registration functionality

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, redirect, request, url_for
 from flask_login import login_user, logout_user, login_required
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,6 +20,17 @@ def login():
 		print('Failed to log in')
 
 	return render_template('auth/login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('auth.login'))
+
+	return render_template('auth/register.html', form=form)
 
 @auth.route('/logout')
 @login_required
